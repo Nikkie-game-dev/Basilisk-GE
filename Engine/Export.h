8fce3b883b4b5281051b8521b79ef4c0bed6065d
Nikkie-game-dev <nikkiegallardo@outlook.com.ar>
2025-08-19 12:55:32 -0300

    add dll export macros

---
+++ Engine/Export.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#ifdef _WIN32
+    #ifdef BASILISK_EXPORT
+        #define BASILISK_API __declspec(dllexport)
+    #else
+        #define BASILISK_API __declspec(dllimport)
+    #endif
+#endif

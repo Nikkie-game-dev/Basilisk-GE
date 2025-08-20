8d1a72d78475f41f4567ead1787d3101b91a4ecd
Nikkie-game-dev <nikkiegallardo@outlook.com.ar>
2025-08-19 21:01:15 -0300

    add vao logic to main

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -1,9 +1,20 @@
-#include <glfw3.h>
+#define GLFW_INCLUDE_NONE
 
-int main(void)
+#include <glad/gl.h> // needed because glfw3 only includes opengl 1.2
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+
+
+int main()
 {
     GLFWwindow* window;
+    int success;
 
+
+    /*Buffers*/
+    GLuint vao; // Vertex Array Object
+    
     /* Initialize the library */
     if (!glfwInit())
         return -1;
@@ -19,11 +30,24 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /*Init glad*/
+    if (!gladLoadGL(glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    
+    /*VAO gen*/
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);

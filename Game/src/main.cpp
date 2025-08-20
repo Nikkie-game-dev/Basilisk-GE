132305326e1c3960dc462f409ac0f4b322997ab8
Nikkie-game-dev <nikkiegallardo@outlook.com.ar>
2025-08-19 21:04:59 -0300

    add vertex shader

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -16,6 +16,15 @@ int main()
     GLuint vao; // Vertex Array Object
     GLuint vbo; // Vertex Buffer Object
 
+    /*Shaders*/
+    const char* vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+
+
     /* Initialize the library */
     if (!glfwInit())
         return -1;
@@ -28,7 +37,10 @@ int main()
         0.5f, -0.5f, 0.0f,
         0.0f, 0.5f, 0.0f
     };
-    
+
+    /*Vertex Shader Object*/
+    unsigned int vertexShader;
+
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -57,7 +69,27 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    /*Create vso and compile*/
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        abort();
+    }
+
+
     
+    /*Deletion*/
+    glDeleteShader(vertexShader);
+
+    /*Vertex Attribute*/
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {

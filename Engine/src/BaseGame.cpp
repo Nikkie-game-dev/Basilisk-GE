452fa1670606bb906925d6efaa4f5f3264caa3ab
Nikkie-game-dev <nikkiegallardo@outlook.com.ar>
2025-09-06 19:04:08 -0300

    use proper setgl function stead of calling window hint
    Signed-off-by: Nikkie-game-dev <nikkiegallardo@outlook.com.ar>

--- Engine/src/BaseGame.cpp
+++ Engine/src/BaseGame.cpp
@@ -10,9 +10,7 @@ namespace basilisk
     {
         Renderer.InitGLFW();
 
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        Renderer.SetGlVersion();
 
         this->Window = new basilisk::Window(windowName, sizeX, sizeY);
         

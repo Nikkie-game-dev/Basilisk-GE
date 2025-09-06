573f3f6f2df11aefd82c0498adf21d4a3e406d4d
Nikkie-game-dev <nikkiegallardo@outlook.com.ar>
2025-09-06 15:14:08 -0300

    reformat export region

--- Engine/src/BaseGame.h
+++ Engine/src/BaseGame.h
@@ -1,15 +1,16 @@
 #pragma once
 
-
+#pragma region Export
 #ifdef _WIN32
-#ifdef BASILISK_EXPORT
-#define BASILISK_API __declspec(dllexport)
-#define BASILISK_EXTERN
-#else
-#define BASILISK_API __declspec(dllimport)
-#define BASILISK_EXTERN extern
+    #ifdef BASILISK_EXPORT
+        #define BASILISK_API __declspec(dllexport)
+        #define BASILISK_EXTERN
+    #else
+        #define BASILISK_API __declspec(dllimport)
+        #define BASILISK_EXTERN extern
+    #endif
 #endif
-#endif
+#pragma endregion
 
 //class Window;
 

020cf10a0e804983e262b8f14530488e9125bed7
Nikkie-game-dev <nikkiegallardo@outlook.com.ar>
2025-09-06 19:12:27 -0300

    add example to Game
    Signed-off-by: Nikkie-game-dev <nikkiegallardo@outlook.com.ar>

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -1,4 +1,14 @@
+#include "Basilisk/BaseGame.h"
 
 int main()
 {
+    auto engine = basilisk::BaseGame("Test", 600, 600);
+    engine.BuildShader();
+    
+    while (!engine.WindowShouldClose())
+    {
+        engine.StartDraw();
+        engine.Draw();
+        engine.EndDraw();
+    }
 }

4aabb5019c26e09e61b3288742933b5a69da7ecd
Nikkie-game-dev <nikkiegallardo@outlook.com.ar>
2025-09-13 19:25:23 -0300

    fix wrong type for colors
    Signed-off-by: Nikkie-game-dev <nikkiegallardo@outlook.com.ar>

--- Engine/src/Colors.h
+++ Engine/src/Colors.h
@@ -42,13 +42,12 @@ namespace basilisk
 
     // Color palette from https://coolors.co/palette/f00314-ff8019-fae603-28e10a-3bb5ff-0500c7-5c03fa-de00ed
 
-    constexpr static auto Red = Color(255, 3, 20);
-    constexpr static auto Orange = Color(255, 128, 25);
-    constexpr static auto Yellow = Color(250, 230, 3);
-    constexpr static auto Green = Color(40, 225, 10);
-    constexpr static auto Cyan = Color(59, 181, 255);
-    constexpr static auto DeepBlue = Color(5, 0, 199);
-    constexpr static auto Blue = Color(92, 3, 250);
-    constexpr static auto Purple = Color(222, 0, 237);
-    
+    const static auto Red = Color(255, 3, 20);
+    const static auto Orange = Color(255, 128, 25);
+    const static auto Yellow = Color(250, 230, 3);
+    const static auto Green = Color(40, 225, 10);
+    const static auto Cyan = Color(59, 181, 255);
+    const static auto DeepBlue = Color(5, 0, 199);
+    const static auto Blue = Color(92, 3, 250);
+    const static auto Purple = Color(222, 0, 237);
 }// namespace basilisk

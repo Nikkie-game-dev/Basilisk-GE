01a90e557046631bf2258e31fb5f1a85626e1e2c
sofi <genuinedistinctclearcapital@gmail.com>
2025-09-14 15:15:12 -0300

    move general entity logic to entity instead of entity2D

--- Engine/src/Entity.cpp
+++ Engine/src/Entity.cpp
@@ -1,6 +1,8 @@
 #include "Entity.h"
-
+#include "GL/glew.h"
 #include "Renderer.h"
+#include "glm/gtc/type_ptr.hpp"
+#include "Entity2D.h"
 
 namespace basilisk
 {
@@ -19,7 +21,7 @@ namespace basilisk
             Renderer::GetInstance().Draw(this->AmountIndices);
         }
     }
-    
+
     void Entity::UpdateBuffers() const
     {
         Renderer::GetInstance().GenerateVBs(this->Vertices, this->Indices, this->AmountVertices, this->AmountIndices);
@@ -29,33 +31,126 @@ namespace basilisk
     {
         return this->ModelMatrix;
     }
-    
+
     void Entity::FillVertices(float vertices[], const int amountVertices)
     {
         delete[] this->Vertices;
-        
+
         this->Vertices = new float[amountVertices];
 
         for (int i = 0; i < amountVertices; ++i)
         {
-            this->Vertices[i]  = vertices[i];
+            this->Vertices[i] = vertices[i];
         }
 
         this->AmountVertices = amountVertices;
     }
-    
+
     void Entity::FillIndices(unsigned int indices[], const int amountIndices)
     {
         delete[] this->Indices;
-        
+
         this->Indices = new unsigned int[amountIndices];
 
         for (int i = 0; i < amountIndices; ++i)
         {
-            this->Indices[i]  = indices[i];
+            this->Indices[i] = indices[i];
         }
-        
+
         this->AmountIndices = amountIndices;
-        
+    }
+
+    /// <summary>
+    /// Negative values rotate counter clockwise
+    /// </summary>
+    /// <param name="rotation"></param>
+    void Entity::SetRotation(float angle, Axis rotationAxis)
+    {
+        switch (rotationAxis)
+        {
+        case basilisk::Axis::X:
+            this->Rotation.x = angle;
+            break;
+        case basilisk::Axis::Y:
+            this->Rotation.y = angle;
+            break;
+        case basilisk::Axis::Z:
+            this->Rotation.z = angle;
+            break;
+        default:
+            this->Rotation.z = angle;
+            break;
+        }
+
+        UpdateRotationMatrix();
+        UpdateModelMatrix();
+    }
+
+    void Entity::SetScaling(glm::vec3 scaling)
+    {
+        this->Scaling = scaling;
+        this->ScaleMatrix = glm::scale(glm::mat4(1.0f), this->Scaling);
+        UpdateModelMatrix();
+    }
+
+    void Entity::SetScaling(float scale, Axis scalingAxis)
+    {
+        switch (scalingAxis)
+        {
+        case basilisk::Axis::X:
+            this->Scaling.x = scale;
+            break;
+        case basilisk::Axis::Y:
+            this->Scaling.y = scale;
+            break;
+        case basilisk::Axis::Z:
+            this->Scaling.z = scale;
+            break;
+        default:
+            break;
+        }
+
+        SetScaling(this->Scaling);
+    }
+
+    /// <summary>
+    /// Position is the top left corner of the entity
+    /// </summary>
+    /// <param name="newPosition"></param>
+    void Entity::SetPosition(glm::vec3 newPosition)
+    {
+        this->Position = newPosition;
+
+        UpdateTranslateMatrix();
+        UpdateModelMatrix();
+    }
+
+    void Entity::UpdateRotationMatrix()
+    {
+        this->RotationMatrix = glm::mat4(1.0f);
+        if (Rotation.x != 0.0f)
+            this->RotationMatrix = glm::rotate(this->RotationMatrix, glm::radians(this->Rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
+
+        if (Rotation.y != 0.0f)
+            this->RotationMatrix = glm::rotate(this->RotationMatrix, glm::radians(this->Rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
+
+        if (Rotation.z != 0.0f)
+            this->RotationMatrix = glm::rotate(this->RotationMatrix, glm::radians(this->Rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
+    }
+
+    void Entity::UpdateTranslateMatrix()
+    {
+        this->TranslateMatrix = glm::translate(glm::mat4(1.0f), this->Position);
+    }
+
+    void Entity::UpdateModelMatrix()
+    {
+        this->ModelMatrix = this->TranslateMatrix * this->RotationMatrix * this->ScaleMatrix;
+        this->Position = glm::vec3(this->ModelMatrix[3]);
+        auto rotation = glm::quat_cast(this->ModelMatrix);
+        this->Rotation = glm::eulerAngles(rotation);
+        this->Scaling = {glm::length(glm::vec3(this->ModelMatrix[0])), 
+                         glm::length(glm::vec3(this->ModelMatrix[1])),
+                         glm::length(glm::vec3(this->ModelMatrix[2]))};
     }
 } // namespace basilisk
--- Game/lib/include/Basilisk/Entity2D.h
+++ Game/lib/include/Basilisk/Entity2D.h
@@ -3,12 +3,7 @@
 
 namespace basilisk
 {
-    enum class Axis : char
-    {
-        X,
-        Y,
-    };
-
+    
     class BASILISK_API Entity2D : public Entity
     {
     public:
@@ -20,21 +15,9 @@ namespace basilisk
 
         virtual void SetRotation(float angle);
         virtual void SetScaling(glm::vec2 scaling);
-        virtual void SetScaling(float scale, Axis scalingAxis);
         virtual void SetPosition(glm::vec2 newPosition);
+
         virtual void Update() override;
 
-    
-
-    private:
-        glm::vec2 Position = {0, 0};
-        glm::vec2 Scaling = {1, 1};
-        float Rotation = 0.0f;
-        glm::mat4 TranslateMatrix = glm::mat4(1.0f);
-        glm::mat4 ScaleMatrix = glm::mat4(1.0f);
-        glm::mat4 RotationMatrix = glm::mat4(1.0f);
-        void UpdateRotationMatrix();
-        void UpdateTranslateMatrix();
-        void UpdateModelMatrix();
     };
 } // namespace basilisk

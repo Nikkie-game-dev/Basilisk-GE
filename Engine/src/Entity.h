0a5421acc2724bf15d4f69c5a11ab01eb03cd1cf
sofi <genuinedistinctclearcapital@gmail.com>
2025-09-14 15:15:12 -0300

    move general entity logic to entity instead of entity2D
    I played around with the values and figured out how to do 3D logic, though it's not visually represented

--- Engine/src/Entity.h
+++ Engine/src/Entity.h
@@ -5,6 +5,14 @@
 
 namespace basilisk
 {
+    enum class Axis : char
+    {
+        X,
+        Y,
+        Z
+    };
+
+
     class BASILISK_API Entity
     {
     public:
@@ -19,18 +27,31 @@ namespace basilisk
         void UpdateBuffers() const;
         void FillVertices(float vertices[], int amountVertices);
         void FillIndices(unsigned int indices[], int amountIndices);
-        
+        void SetRotation(float angle, Axis rotationAxis);
+        void SetScaling(glm::vec3 scaling);
+        void SetScaling(float scale, Axis scalingAxis);
+        void SetPosition(glm::vec3 newPosition);
+
         bool IsActive = true;
         glm::mat4 GetModelMatrix() const;
 
     protected:
         glm::mat4 ModelMatrix = glm::mat4(1.0f);
+        glm::vec3 Position = {0, 0, 0};
+        glm::vec3 Scaling = {1, 1, 1};
+        glm::vec3 Rotation = {0, 0, 0};
+        glm::mat4 TranslateMatrix = glm::mat4(1.0f);
+        glm::mat4 ScaleMatrix = glm::mat4(1.0f);
+        glm::mat4 RotationMatrix = glm::mat4(1.0f);
 
     private:
         float* Vertices = nullptr;
         unsigned int* Indices = nullptr;
         int AmountVertices = 0;
         int AmountIndices = 0;
-        
+
+        void UpdateRotationMatrix();
+        void UpdateTranslateMatrix();
+        void UpdateModelMatrix();
     };
 } // basilisk 
--- Engine/src/Entity2D.cpp
+++ Engine/src/Entity2D.cpp
@@ -1,13 +1,12 @@
 #include "Entity2D.h"
-#include "glm/gtc/type_ptr.hpp"
-#include "GL/glew.h"
+
 #include "Renderer.h"
 
 namespace basilisk
 {
     float Entity2D::GetRotation() const
     {
-        return this->Rotation;
+        return this->Rotation.z;
     }
 
     glm::vec2 Entity2D::GetScale() const
@@ -20,64 +19,19 @@ namespace basilisk
         return this->Position;
     }
 
-    /// <summary>
-    /// Negative values rotate counter clockwise
-    /// </summary>
-    /// <param name="rotation"></param>
     void Entity2D::SetRotation(float angle)
     {
-        this->Rotation = angle;
-        UpdateRotationMatrix();
-        UpdateModelMatrix();
+        this->Entity::SetRotation(angle, Axis::Z);
     }
 
     void Entity2D::SetScaling(glm::vec2 scaling)
     {
-        this->Scaling = scaling;
-        this->ScaleMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(this->Scaling, 1.0f));
-        UpdateModelMatrix();
+        this->Entity::SetScaling({scaling.x, scaling.y, 1.0f});
     }
 
-    void Entity2D::SetScaling(float scale, Axis scalingAxis)
-    {
-        if (scalingAxis == Axis::X)
-            this->Scaling.x = scale;
-        else
-            this->Scaling.y = scale;
-
-        SetScaling(this->Scaling);
-    }
-
-    /// <summary>
-    /// Position is the top left corner of the entity
-    /// </summary>
-    /// <param name="newPosition"></param>
     void Entity2D::SetPosition(glm::vec2 newPosition)
     {
-        this->Position = newPosition;
-
-        UpdateTranslateMatrix();
-        UpdateModelMatrix();
-    }
-
-    void Entity2D::UpdateRotationMatrix()
-    {
-        this->RotationMatrix = glm::mat4(1.0f);
-        this->RotationMatrix = glm::rotate(this->RotationMatrix, glm::radians(this->Rotation), glm::vec3(0.0f, 0.0f, 1.0f));
-    }
-
-    void Entity2D::UpdateTranslateMatrix()
-    {
-        this->TranslateMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(this->Position, 0.0f));
-    }
-
-    void Entity2D::UpdateModelMatrix()
-    {
-        this->ModelMatrix = this->TranslateMatrix * this->RotationMatrix * this->ScaleMatrix;
-        this->Position = glm::vec2(this->ModelMatrix[3]);
-        auto rotation = glm::quat_cast(this->ModelMatrix);
-        this->Rotation = glm::eulerAngles(rotation).z;
-        this->Scaling = glm::vec2(glm::length(glm::vec2(this->ModelMatrix[0])), glm::length(glm::vec2(this->ModelMatrix[1])));
+        this->Entity::SetPosition({newPosition.x, newPosition.y, 0.0f});
     }
 
     void Entity2D::Update()

d72a8831b27c2ab088099cd1ed29bd76c623318a
Nikkie-game-dev <nikkiegallardo@outlook.com.ar>
2025-09-24 17:19:38 -0300

    adapt base game to catch errors

--- Engine/src/BaseGame.cpp
+++ Engine/src/BaseGame.cpp
@@ -1,20 +1,32 @@
 #include "BaseGame.h"
 #include <glm/glm.hpp>
+#include <iostream>
+
 #include "Renderer.h"
 #include "Window.h"
 
 namespace basilisk
 {
 
-    BaseGame::BaseGame(const std::string& windowName, int sizeX, int sizeY) : Renderer(Renderer::GetInstance()), X(sizeX), Y(sizeY)
+    BaseGame::BaseGame(const std::string& windowName, const int sizeX, const int sizeY) :
+        Renderer(Renderer::GetInstance()), X(sizeX), Y(sizeY)
     {
-        Renderer.InitGLFW();
+        try
+        {
+            Renderer.InitGLFW();
 
-        Renderer.SetGlVersion();
+            Renderer.SetGlVersion();
 
-        this->Window = new basilisk::Window(windowName, glm::ivec2(sizeX, sizeY));
+            this->Window = new basilisk::Window(windowName, glm::ivec2(sizeX, sizeY));
 
-        Renderer.InitGL(glm::ivec2(sizeX, sizeY));
+            Renderer.InitGL(glm::ivec2(sizeX, sizeY));
+
+            Renderer.SetWindowRef(*this->Window);
+        }
+        catch (std::exception& error)
+        {
+            std::cerr << error.what();
+        }
     }
 
     BaseGame::~BaseGame()
@@ -22,66 +34,110 @@ namespace basilisk
         delete this->Window;
     }
 
-    BaseGame::BaseGame(const BaseGame& other) : X(other.X), Y(other.Y), Renderer(Renderer::GetInstance())
+    BaseGame::BaseGame(const BaseGame& other) :
+        Renderer(Renderer::GetInstance()), X(other.X), Y(other.Y)
     {
-        this->WindowName = other.WindowName;
-        this->Window = new basilisk::Window(other.WindowName, glm::vec2(other.X, other.Y));
-    }
-
-    BaseGame::BaseGame(BaseGame&& other) noexcept : Renderer(Renderer::GetInstance()), X(other.X), Y(other.Y)
-    {
-        this->WindowName = other.WindowName;
-        this->Window = other.Window;
-        other.Window = nullptr;
-        other.X = 0;
-        other.Y = 0;
-        other.WindowName = "";
-    }
-
-    BaseGame& BaseGame::operator=(const BaseGame& other)
-    {
-        if (this != &other)
+        try
         {
-            this->X = other.X;
-            this->Y = other.Y;
             this->WindowName = other.WindowName;
             this->Window = new basilisk::Window(other.WindowName, glm::vec2(other.X, other.Y));
+            Renderer.SetWindowRef(*this->Window);
         }
-        return *this;
+        catch (std::exception& error)
+        {
+            std::cerr << error.what();
+        }
     }
 
-    BaseGame& BaseGame::operator=(BaseGame&& other) noexcept
+    BaseGame::BaseGame(BaseGame&& other) noexcept :
+        Renderer(Renderer::GetInstance()), X(other.X), Y(other.Y)
     {
-        if (this != &other)
+        try
         {
-            this->X = other.X;
-            this->Y = other.Y;
             this->WindowName = other.WindowName;
             this->Window = other.Window;
-
             other.Window = nullptr;
             other.X = 0;
             other.Y = 0;
             other.WindowName = "";
+            Renderer.SetWindowRef(*this->Window);
+        }
+        catch (std::exception& error)
+        {
+            std::cerr << error.what();
+        }
+    }
+
+    BaseGame& BaseGame::operator=(const BaseGame& other)
+    {
+        try
+        {
+            if (this != &other)
+            {
+                this->X = other.X;
+                this->Y = other.Y;
+                this->WindowName = other.WindowName;
+                this->Window = new basilisk::Window(other.WindowName, glm::vec2(other.X, other.Y));
+                Renderer.SetWindowRef(*this->Window);
+            }
+        }
+        catch (std::exception& error)
+        {
+            std::cerr << error.what();
+        }
+
+        return *this;
+
+    }
+
+    BaseGame& BaseGame::operator=(BaseGame&& other) noexcept
+    {
+        try
+        {
+            if (this != &other)
+            {
+                this->X = other.X;
+                this->Y = other.Y;
+                this->WindowName = other.WindowName;
+                this->Window = other.Window;
+
+                other.Window = nullptr;
+                other.X = 0;
+                other.Y = 0;
+                other.WindowName = "";
+
+                Renderer.SetWindowRef(*this->Window);
+            }
+        }
+        catch (std::exception& error)
+        {
+            std::cerr << error.what();
         }
         return *this;
     }
 
     void BaseGame::Run()
     {
-        Init();
-
-        Renderer.LoadProjectionMatrix();
-        
-        while (!WindowShouldClose())
+        try
         {
-            Update();
-            Renderer.StartDraw();
-            Draw();
-            Renderer.EndDraw();
-        }
+            Init();
 
-        Close();
+            Renderer.LoadProjectionMatrix();
+
+            while (!WindowShouldClose())
+            {
+                Update();
+                Renderer.StartDraw();
+                Draw();
+                Renderer.EndDraw();
+            }
+
+            Close();
+        }
+        catch (std::exception& error)
+        {
+            std::cerr << error.what();
+        }
     }
 
     bool BaseGame::WindowShouldClose() const

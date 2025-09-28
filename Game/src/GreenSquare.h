fdc88813551e4662212eed0a90ce620b2e150710
sofi <genuinedistinctclearcapital@gmail.com>
2025-09-28 20:45:13 -0300

    make green square class

---
+++ Game/src/GreenSquare.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Basilisk/Square.h"
+
+class GreenSquare : public basilisk::Square
+{
+    GreenSquare(const glm::vec2& size, const glm::vec2& position, float rotation);
+    GreenSquare(const glm::vec2& size);
+    void Update() override;
+};

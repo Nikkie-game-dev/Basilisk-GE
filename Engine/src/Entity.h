84648f8e43887bb2672691aa0d3608b5d48f3935
Nikkie-game-dev <nikkiegallardo@outlook.com.ar>
2025-09-28 19:49:02 -0300

    add documentation for entity
    Signed-off-by: Nikkie-game-dev <nikkiegallardo@outlook.com.ar>

--- Engine/src/Entity.h
+++ Engine/src/Entity.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "Colors.h"
 #include "Export.h"
 #include "Material.h"
 #include "glm/glm.hpp"
@@ -18,26 +17,62 @@ namespace basilisk
     class BASILISK_API Entity
     {
     public:
-        Entity() = default;
+        Entity();
         virtual ~Entity();
-
+        
+#pragma region Abstracts
+        /// <summary>
+        /// Initializes the entity. Should be called inside an implemented Init call from base game.
+        /// </summary>
         virtual void Init() = 0;
+
+        /// <summary>
+        /// Updates the entity. Should be called inside an implemented Update call from base game.
+        /// </summary>
         virtual void Update() = 0;
+
+        /// <summary>
+        /// Draws the entity. Should be called inside an implemented Draw call from base game.
+        /// </summary>
         virtual void Draw() = 0;
         
+#pragma endregion
+
+#pragma region Setters
+        /// <summary>
+        /// Sets and rotates an entity.
+        /// </summary>
+        /// <param name="angle">Angle to rotate the entity, counterclockwise</param>
+        /// <param name="rotationAxis">Axis to which rotate</param> 
         void SetRotation(float angle, Axis rotationAxis);
+
+        /// <summary>
+        /// Sets scale vector and scales accordingly.
+        /// </summary>
+        /// <param name="scaling">Saling Vector</param>
         void SetScaling(const glm::vec3& scaling);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="scale">Size </param>
+        /// <param name="scalingAxis"></param>
         void SetScaling(float scale, Axis scalingAxis);
         void SetPosition(const glm::vec3& newPosition);
         
         void SetMaterial(const std::shared_ptr<Material>& material);
 
+#pragma endregion
+
+#pragma region Getters
         [[nodiscard]] virtual glm::vec3 GetPosition() const;
         [[nodiscard]] virtual glm::vec3 GetScale() const;
         [[nodiscard]] virtual glm::vec3 GetRotation() const;
 
         [[nodiscard]] std::shared_ptr<Material> GetMaterial() const;
         [[nodiscard]] glm::mat4 GetModelMatrix() const;
+        
+#pragma endregion
         
         bool IsActive = true;
 

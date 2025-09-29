e13f4a5a8dcd0d91d92d7cdca537efabc4869d78
Nikkie-game-dev <nikkiegallardo@outlook.com.ar>
2025-09-29 14:18:13 -0300

    add documentation to buffers
    Signed-off-by: Nikkie-game-dev <nikkiegallardo@outlook.com.ar>

--- Engine/src/Buffers.h
+++ Engine/src/Buffers.h
@@ -3,12 +3,32 @@
 
 namespace basilisk
 {
+    
     struct BASILISK_API Buffers
     {
+        /// <summary>
+        /// Vertex Buffer Object. Stores vertex data.
+        /// </summary>
         unsigned int Vbo = 0;
+
+        /// <summary>
+        /// Vertex Array Object. Stores all the attributes needed to supply vertex data 
+        /// </summary>
         unsigned int Vao = 0;
+
+        /// <summary>
+        /// Element Buffer Object. Stores indices.
+        /// </summary>
         unsigned int Ebo = 0;
+
+        /// <summary>
+        /// Pointer to an array of vertices.
+        /// </summary>
         float* Vertices = nullptr;
+
+        /// <summary>
+        /// Pointer to an array of indices.
+        /// </summary>
         unsigned int* Indices = nullptr;
         int AmountVertices;
         int AmountIndices;

daae907c5d309c54461b2a6370465f74d6724149
sofi <genuinedistinctclearcapital@gmail.com>
2025-10-07 18:33:22 -0300

    add vertices to sprite

--- Engine/src/Sprite.cpp
+++ Engine/src/Sprite.cpp
@@ -1,8 +1,31 @@
 #include "Sprite.h"
+#include "TextureImporter.h"
 
 namespace basilisk
 {
-    Sprite::Sprite()
+    Sprite::Sprite(std::string textureDir)
     {
+        this->texture = TextureImporter::GetTexture(textureDir);
+
+        float vertices[] = 
+        {
+            // positions          // colors           // texture coords
+            0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
+            0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+            -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f // top left
+        };
+
+        unsigned int indices[]
+        {
+            0, 1, 3, // first triangle
+            1, 2, 3 // second triangle
+        };
+
+        this->FillVertices(vertices, sizeof(vertices));
+        this->FillIndices(indices, sizeof(indices));
     }
+
+
+
 } // namespace basilisk

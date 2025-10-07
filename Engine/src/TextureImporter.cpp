86985a42b50a0a729362c237aeb3d26370b470f4
sofi <genuinedistinctclearcapital@gmail.com>
2025-10-07 17:55:39 -0300

    make texture importer methods

--- Engine/src/TextureImporter.cpp
+++ Engine/src/TextureImporter.cpp
@@ -1,8 +1,60 @@
 #include "TextureImporter.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
 
 namespace basilisk
 {
+    TextureImporter::TextureImporter()
+    {
+    }
 
-}
+    unsigned int TextureImporter::GetTexture(std::string imageDir)
+    {
+        int width;
+        int height;
+        auto data = ImportImage(imageDir, width, height);
+        auto texture = GenTexture(data, width, height);
+
+        FreeImage(data);
+    }
+
+    unsigned char* TextureImporter::ImportImage(std::string imageDir, int& width, int& height)
+    {
+        int colorChannelsAmount;
+        auto* data = stbi_load(imageDir.c_str(), &width, &height, &colorChannelsAmount, 0);
+
+        if (!data)
+            throw FailedTextureLoading();
+
+        return data;
+    }
+
+    unsigned int TextureImporter::GenTexture(unsigned char* data, int &width, int& height)
+    {
+        unsigned int texture;
+
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        return texture;
+    }
+
+    void TextureImporter::FreeImage(unsigned char* data)
+    {
+        if (!data)
+            return;
+
+        stbi_image_free(data);
+    }
+
+} // namespace basilisk
--- Engine/src/TextureImporter.h
+++ Engine/src/TextureImporter.h
@@ -1,7 +1,27 @@
 #pragma once
+#include <string>
+#include "Export.h"
+
 namespace basilisk
 {
-    class TextureImporter
+    class BASILISK_API TextureImporter
     {
+    public:
+        TextureImporter();
+        static unsigned int GetTexture(std::string imageDir);
+
+    private:
+        unsigned char* ImportImage(std::string imageDir, int& width, int& height);
+        unsigned int GenTexture(unsigned char* data, int& width, int& height);
+        void FreeImage(unsigned char* data);
+    };
+
+    class FailedTextureLoading : std::exception
+    {
+    public:
+        [[nodiscard]] char const* what() const override
+        {
+            return "Tried and failed load a texture";
+        }
     };
 } // namespace basilisk

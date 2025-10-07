f7b19c2fde0bd7f7eb63a335136f7d07119f3efa
franze-dev <genuinedistinctclearcapital@gmail.com>
2025-10-07 10:17:21 -0300

    test square transforming

--- Game/src/GreenSquare.cpp
+++ Game/src/GreenSquare.cpp
@@ -9,5 +9,27 @@ namespace game
 
     void GreenSquare::Update()
     {
+        auto rot = GetRotation2D();
+        SetRotation(rot - 30);
+
+        static bool growing = false;
+        auto scale = GetScale2D();
+        if (scale.x > 50)
+        {
+            growing = false;
+        }
+        else if (scale.x < 10)
+        {
+            growing = true;
+        }
+
+        if (growing)
+        {
+            SetScaling({scale.x + 1, scale.y + 1});
+        }
+        else
+        {
+            SetScaling({scale.x - 1, scale.y - 1});
+        }
     }
 } // namespace game

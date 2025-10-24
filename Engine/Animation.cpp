b0979d596806dc7f6617311ff5ef953193904d6c
sofi <genuinedistinctclearcapital@gmail.com>
2025-10-24 18:42:09 -0300

    make frame adding logic
    you can now add frames to the animation

--- Engine/Animation.cpp
+++ Engine/Animation.cpp
@@ -13,3 +13,34 @@ void Animation::Update(float delta)
 
     CurrentFrame = static_cast<int>(CurrentTime / frameLength);
 }
+
+void Animation::AddFrame(glm::vec2 frameCoord, glm::vec2 frameSize, glm::vec2 textureSize, float duration)
+{
+    AddFrame(frameCoord, frameSize, textureSize, duration, 1);
+}
+void Animation::AddFrame(glm::vec2 frameCoord, glm::vec2 frameSize, glm::vec2 textureSize, float duration, int frameCount)
+{
+    Length = duration * 1000;
+
+    float currentXPos = 0;
+
+    for (int i = 0; i < frameCount; i++)
+    {
+        Frame frame;
+        frame.coords[0].u = (frameCoord.x + currentXPos) / textureSize.x;
+        frame.coords[0].v = frameCoord.y / textureSize.y;
+
+        frame.coords[1].u = (frameCoord.x + currentXPos + frameSize.x) / textureSize.x;
+        frame.coords[1].v = frame.coords[0].v;
+
+        frame.coords[2].u = frame.coords[0].u;
+        frame.coords[2].v = (frameCoord.y + frameSize.y + currentXPos) / textureSize.y;
+
+        frame.coords[3].u = frame.coords[1].u;
+        frame.coords[3].v = frame.coords[2].v;
+
+        Frames.push_back(frame);
+
+        currentXPos += frameSize.x;
+    }
+}
--- Engine/Animation.h
+++ Engine/Animation.h
@@ -9,6 +9,20 @@ class Animation
 public:
     void Update(float delta);
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="frameCoord"></param>
+    /// <param name="frameSize"></param>
+    /// <param name="textureSize"></param>
+    /// <param name="duration">Frame duration in seconds</param>
+    void AddFrame(glm::vec2 frameCoord, glm::vec2 frameSize, 
+                  glm::vec2 textureSize, float duration);
+
+    void AddFrame(glm::vec2 frameCoord, glm::vec2 frameSize, 
+                  glm::vec2 textureSize, float duration, 
+                  int frameCount);
+
 private:
     int CurrentFrame;
     float CurrentTime;

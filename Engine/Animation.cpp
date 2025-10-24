3592b8a0be18aa409d015cd73e06764a97572501
sofi <genuinedistinctclearcapital@gmail.com>
2025-10-24 18:40:10 -0300

    make Animation update logic
    it checks which frame is being shown on screen

--- Engine/Animation.cpp
+++ Engine/Animation.cpp
@@ -1,1 +1,15 @@
 #include "Animation.h"
+
+void Animation::Update(float delta)
+{
+    CurrentTime += delta;
+
+    while (CurrentTime > Length)
+    {
+        CurrentTime -= Length;
+    }
+
+    float frameLength = Length / Frames.size();
+
+    CurrentFrame = static_cast<int>(CurrentTime / frameLength);
+}
--- Engine/Animation.h
+++ Engine/Animation.h
@@ -1,5 +1,17 @@
 #pragma once
+#include <vector>
+#include "glm/glm.hpp"
+
+#include "Frame.h"
 
 class Animation
 {
+public:
+    void Update(float delta);
+
+private:
+    int CurrentFrame;
+    float CurrentTime;
+    float Length;
+    std::vector<Frame> Frames;
 };

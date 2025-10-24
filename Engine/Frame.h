d24f7fe62cc02a3908cc1add36be91480cde6cd7
sofi <genuinedistinctclearcapital@gmail.com>
2025-10-24 18:38:00 -0300

    add UV coordinates to Frame

--- Engine/Frame.h
+++ Engine/Frame.h
@@ -1,5 +1,14 @@
 #pragma once
 class Frame
 {
-    
+    struct UVCoords
+    {
+        float u;
+        float v;
+    };
+
+public:
+    Frame() = default;
+    ~Frame() = default;
+    UVCoords coords[4];
 };

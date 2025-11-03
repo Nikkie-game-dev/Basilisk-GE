f8321729929451b64913251d48de2398ab9533d2
Nikkie-game-dev <nikkiegallardo@outlook.com.ar>
2025-11-03 18:41:26 -0300

    add constructors and destructors to Sprite
    Signed-off-by: Nikkie-game-dev <nikkiegallardo@outlook.com.ar>

--- Engine/src/Sprite.h
+++ Engine/src/Sprite.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Animation.h"
 #include "Entity2D.h"
 
 namespace basilisk
@@ -7,10 +8,19 @@ namespace basilisk
     {
     public:
         explicit Sprite(const std::string& textureDir, glm::vec2 center, glm::vec2 size);
+        ~Sprite();
+
+        Sprite(const Sprite& Other); // copy constructor
+        Sprite(Sprite&& Other) noexcept; // move constructor
+        Sprite& operator=(const Sprite& Other); // copy assignment
+        Sprite& operator=(Sprite&& Other) noexcept; // move assignment
+        
         void Init() override;
+        void UpdateAnim();
 
     private:
         unsigned int Texture;
+        Animation* Anim = nullptr;
     };
 
 } // namespace basilisk

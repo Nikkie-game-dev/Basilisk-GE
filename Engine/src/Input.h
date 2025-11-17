ea5b8b6aa9e5133213301c99958040b865fcc35c
Nikkie-game-dev <nikkiegallardo@outlook.com.ar>
2025-11-17 14:14:21 -0300

    add documentation to input
    Signed-off-by: Nikkie-game-dev <nikkiegallardo@outlook.com.ar>

--- Engine/src/Input.h
+++ Engine/src/Input.h
@@ -8,16 +8,39 @@
 namespace basilisk
 {
     class Window;
-    
+    /// <summary>
+    /// Input system that handles updating and constructing all input actions.
+    /// </summary>
     class BASILISK_API Input
     {
     public:
+        /// <summary>
+        /// Constructs the input system. It is not needed to create it in a game, as BaseGame already comes with one.
+        /// </summary>
+        /// <param name="window">Window to provide context</param>
         explicit Input(Window* window);
+        
+        /// <summary>
+        /// Destructor.
+        /// </summary>
         ~Input();
 
+        /// <summary>
+        /// Makes a New input action.
+        /// </summary>
+        /// <param name="key">Key to register</param>
+        /// <returns>New Input Action</returns>
         InputAction& NewInput(Keys key);
+
+        /// <summary>
+        /// Stops and removes an input action.
+        /// </summary>
+        /// <param name="inputAction">Input action to remove</param>
         void StopUpdatingInput(InputAction& inputAction);
 
+        /// <summary>
+        /// Updates all registered input action's state.
+        /// </summary>
         void UpdateInputs();
 
     private:

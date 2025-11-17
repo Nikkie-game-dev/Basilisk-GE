968af30e97ec0a5143ff41594635cc553684178f
Nikkie-game-dev <nikkiegallardo@outlook.com.ar>
2025-11-17 14:47:04 -0300

    add Triangle summary
    Signed-off-by: Nikkie-game-dev <nikkiegallardo@outlook.com.ar>

--- Engine/src/Triangle.h
+++ Engine/src/Triangle.h
@@ -5,14 +5,15 @@
 
 namespace basilisk
 {
+    /// <summary>
+    /// Primitive triangle shape. The color can be provided by the Color class or read from material.
+    /// </summary>
     class BASILISK_API Triangle : public Shape
     {
     protected:
         explicit Triangle(glm::vec2 center, glm::vec2 size, bool isSolid, const basilisk::Color& color);
 
     private:
-
-        
         /// <summary>
         /// Initializes the vertices that will be used for drawing the triangle.
         /// </summary>

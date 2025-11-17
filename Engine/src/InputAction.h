ae41ac8f06c138138b95f9a281bf48d30512edba
Nikkie-game-dev <nikkiegallardo@outlook.com.ar>
2025-11-17 14:30:31 -0300

    add documetation for InputAction
    Signed-off-by: Nikkie-game-dev <nikkiegallardo@outlook.com.ar>

--- Engine/src/InputAction.h
+++ Engine/src/InputAction.h
@@ -52,18 +52,48 @@ namespace basilisk
         RIGHT_ALT,
         SHIFT,
         CTRL,
-    };    
+    };
     
+    /// <summary>
+    /// The input itself. It provides access to the keys.
+    /// </summary>
     class BASILISK_API InputAction
     {
     public:
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         InputAction();
+        
+        /// <summary>
+        /// Constructor for the input action. It associates a window and a key to an action. 
+        /// It should not be used by itself, as Input provides a constructor for this class.
+        /// </summary>
+        /// <param name="window">Window to associate context</param>
+        /// <param name="key">Key to associate</param>
         InputAction(Window* window, const Keys& key);
 
+        /// <summary>
+        /// Checks if the key is currently not pressed.
+        /// </summary>
+        /// <returns>True if not pressed</returns>
         [[nodiscard]] bool IsUp() const;
+
+        /// <summary>
+        /// Checks if the key is currently pressed.
+        /// </summary>
+        /// <returns>True if pressed</returns>
         [[nodiscard]] bool IsDown() const;
+
+        /// <summary>
+        /// Checks if the key was not pressed in last frame but now is currently pressed.
+        /// </summary>
+        /// <returns>True if last frame was released and this frame is pressed</returns>
         [[nodiscard]] bool IsPressedAndReleased() const;
 
+        /// <summary>
+        /// Retrieves the key state.
+        /// </summary>
         void Update();
         
     private:

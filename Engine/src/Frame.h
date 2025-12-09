7756e219304aa277ee0fffb808e31b2e67e3d7f8
sofi <genuinedistinctclearcapital@gmail.com>
2025-12-09 12:50:31 -0300

    add missing documentation

--- Engine/src/Frame.h
+++ Engine/src/Frame.h
@@ -9,9 +9,9 @@ namespace basilisk
         /// <summary>
         /// Constructs a frame with a given size.
         /// </summary>
-        /// <param name="bottomLeft"></param>
-        /// <param name="frameSize"></param>
-        /// <param name="textureSize"></param>
+        /// <param name="bottomLeft">Bottom left coordinate of the frame</param>
+        /// <param name="frameSize">Size of the frame (x = width, y = height)</param>
+        /// <param name="textureSize">Size of the texture (x = width, y = height)</param>
         Frame(const glm::vec2& bottomLeft, const glm::vec2& frameSize, const glm::vec2& textureSize);
 
         glm::vec2 TopLeft;
